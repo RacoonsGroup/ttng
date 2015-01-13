9964ee06737657c83b66674c76ff45ebb1c747b8
Andrey Nenashev <proudhamyak@gmail.com>
2015-01-13 14:20:23 +0300

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -18,36 +18,39 @@ ActiveRecord::Schema.define(version: 20141225101753) do
 
   create_table "articles", force: :cascade do |t|
     t.integer  "user_id"
-    t.string   "url",         limit: 255
-    t.string   "description", limit: 255,             null: false
+    t.string   "url"
+    t.string   "description",             null: false
     t.text     "content"
-    t.integer  "importance",              default: 5, null: false
+    t.integer  "importance",  default: 5, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "title",       limit: 255,             null: false
+    t.string   "title",                   null: false
   end
 
   add_index "articles", ["user_id"], name: "index_articles_on_user_id", using: :btree
 
   create_table "customers", force: :cascade do |t|
-    t.string   "name",       limit: 255, null: false
+    t.string   "name",       null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "days", force: :cascade do |t|
     t.date     "date"
-    t.boolean  "holiday",                default: true, null: false
-    t.string   "reason",     limit: 255
+    t.boolean  "holiday",    default: true, null: false
+    t.string   "reason"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
+  add_index "days", ["date"], name: "index_days_on_date", using: :btree
+  add_index "days", ["holiday"], name: "index_days_on_holiday", using: :btree
+
   create_table "project_infos", force: :cascade do |t|
-    t.integer  "project_id",                             null: false
-    t.string   "title",      limit: 255,                 null: false
-    t.text     "info",                                   null: false
-    t.boolean  "encrypted",              default: false, null: false
+    t.integer  "project_id",                 null: false
+    t.string   "title",                      null: false
+    t.text     "info",                       null: false
+    t.boolean  "encrypted",  default: false, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -66,10 +69,10 @@ ActiveRecord::Schema.define(version: 20141225101753) do
   add_index "project_users", ["user_id"], name: "index_project_users_on_user_id", using: :btree
 
   create_table "projects", force: :cascade do |t|
-    t.string   "name",        limit: 255, null: false
-    t.integer  "customer_id",             null: false
+    t.string   "name",        null: false
+    t.integer  "customer_id", null: false
     t.text     "description"
-    t.integer  "rate_kopeks",             null: false
+    t.integer  "rate_kopeks", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "pivotal_id"
@@ -78,15 +81,15 @@ ActiveRecord::Schema.define(version: 20141225101753) do
   add_index "projects", ["customer_id"], name: "index_projects_on_customer_id", using: :btree
 
   create_table "tasks", force: :cascade do |t|
-    t.integer  "user_id",                                 null: false
-    t.integer  "project_id",                              null: false
-    t.string   "name",        limit: 255,                 null: false
+    t.integer  "user_id",                     null: false
+    t.integer  "project_id",                  null: false
+    t.string   "name",                        null: false
     t.text     "description"
-    t.string   "url",         limit: 255
-    t.date     "date",                                    null: false
-    t.boolean  "payable",                 default: false, null: false
-    t.integer  "status",                  default: 0,     null: false
-    t.integer  "task_type",               default: 0,     null: false
+    t.string   "url"
+    t.date     "date",                        null: false
+    t.boolean  "payable",     default: false, null: false
+    t.integer  "status",      default: 0,     null: false
+    t.integer  "task_type",   default: 0,     null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -113,30 +116,30 @@ ActiveRecord::Schema.define(version: 20141225101753) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "",           null: false
-    t.string   "encrypted_password",     limit: 255, default: "",           null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email",                  default: "",           null: false
+    t.string   "encrypted_password",     default: "",           null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0,            null: false
+    t.integer  "sign_in_count",          default: 0,            null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "admin",                              default: false,        null: false
-    t.string   "first_name",             limit: 255, default: "",           null: false
-    t.string   "last_name",              limit: 255, default: "",           null: false
-    t.date     "birth_date",                         default: '2014-12-08', null: false
-    t.integer  "position",                           default: 0,            null: false
-    t.date     "hire_date",                          default: '2014-12-08', null: false
+    t.boolean  "admin",                  default: false,        null: false
+    t.string   "first_name",             default: "",           null: false
+    t.string   "last_name",              default: "",           null: false
+    t.date     "birth_date",             default: '2014-12-22', null: false
+    t.integer  "position",               default: 0,            null: false
+    t.date     "hire_date",              default: '2014-12-22', null: false
     t.date     "fire_date"
-    t.integer  "salary_kopeks",                      default: 0,            null: false
-    t.integer  "official_salary_kopeks",             default: 0,            null: false
-    t.string   "inn",                    limit: 255
-    t.string   "snils",                  limit: 255
-    t.string   "pivotal_token",          limit: 255
+    t.integer  "salary_kopeks",          default: 0,            null: false
+    t.integer  "official_salary_kopeks", default: 0,            null: false
+    t.string   "inn"
+    t.string   "snils"
+    t.string   "pivotal_token"
     t.string   "google_token"
   end
 
