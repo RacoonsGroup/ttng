74d94890b2346869523ba38203f3616cf7cd3648
Anton Zhirkov <an.zhirkov@gmail.com>
2015-04-07 10:45:43 +0300

    Pagination for admin/users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,7 +4,7 @@ class Admin::UsersController < Admin::AdminController
   helper_method :sort_column, :sort_direction
 
   def index
-    @users = @users.order(sort_column + " " + sort_direction).paginate(page: params[:page])
+    @users = @users.order(sort_column + " " + sort_direction).paginate(page: params[:page], per_page: 16)
   end
 
   def edit
