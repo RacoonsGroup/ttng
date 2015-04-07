8981ae0dc014589e95bd770537243d4023cc2eeb
Anton Zhirkov <an.zhirkov@gmail.com>
2015-04-07 10:39:22 +0300

    Sort for admin#users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -41,4 +41,11 @@ module ApplicationHelper
   def iteration_date(date)
     date.strftime('%d.%m')
   end
+
+  def sortable(column, title = nil)
+    title ||= column.titleize
+    css_class = column == sort_column ? "current #{sort_direction}" : nil
+    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    link_to title, {:sort => column, :direction => direction}, {:class => css_class}
+  end
 end
