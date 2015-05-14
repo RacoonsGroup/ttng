0fd2e30d43a7d9faba32c64b57955ee3c660969e
Anton Zhirkov <an.zhirkov@gmail.com>
2015-05-14 16:57:20 +0400

    fix article and customer

--- app/admin/article.rb
+++ app/admin/article.rb
@@ -1,3 +1,3 @@
 ActiveAdmin.register Article do
-  permit_params :title, :description, :url, :content, :importance
+  permit_params :user_id, :title, :description, :url, :content, :importance
 end
--- app/admin/customer.rb
+++ app/admin/customer.rb
@@ -1,3 +1,17 @@
 ActiveAdmin.register Customer do
   permit_params :name, :describe, :url, :profile, :subject, :source, :technology_list
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :source
+      f.input :url
+      f.input :describe
+      f.input :subject, collection: Customer.subjects.keys, as: :select
+      f.input :profile, collection: Customer.profiles.keys, as: :select
+    end
+    f.inputs do
+      f.action :submit
+    end
+  end
 end
