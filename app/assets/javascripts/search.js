8a77f084c996570d5fd984ca0dd14320cb7e4196
Anton Zhirkov <an.zhirkov@gmail.com>
2015-05-18 18:11:50 +0400

    fix i18n for customer and search.js

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,8 +1,4 @@
 $(function() {
-  $("#customers td a, #customers .pagination a").on("click", function() {
-    $.getScript(this.href);
-    return false;
-  });
   $("#customers_search input").keyup(function() {
     $.get($("#customers_search").attr("action"), $("#customers_search").serialize(), null, "script");
     return false;
@@ -10,10 +6,6 @@ $(function() {
 });
 
 $(function() {
-  $("#contacts td a, #contacts .pagination a").on("click", function() {
-    $.getScript(this.href);
-    return false;
-  });
   $("#contacts_search input").keyup(function() {
     $.get($("#contacts_search").attr("action"), $("#contacts_search").serialize(), null, "script");
     return false;
@@ -21,10 +13,6 @@ $(function() {
 });
 
 $(function() {
-  $("#projects td a, #projects .pagination a").on("click", function() {
-    $.getScript(this.href);
-    return false;
-  });
   $("#projects_search input").keyup(function() {
     $.get($("#projects_search").attr("action"), $("#projects_search").serialize(), null, "script");
     return false;
