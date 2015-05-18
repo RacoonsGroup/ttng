a08a10840c4ab02a3978ee41f345f11b971c6005
Anton Zhirkov <an.zhirkov@gmail.com>
2015-05-18 17:09:49 +0400

    Admin can view all projects ib Task#edit

--- app/controllers/related_tasks_controller.rb
+++ app/controllers/related_tasks_controller.rb
@@ -61,7 +61,11 @@ class RelatedTasksController < ApplicationController
 
   def prepare_gon
     gon.related_task = @related_task.present? ? RelatedTaskPresenter.new(@related_task).to_hash : nil
-    gon.projects = current_user.projects
+    gon.projects = if current_user.chief?
+                      Project.all
+                    else
+                      current_user.projects
+                    end
     gon.statuses = RelatedTask.statuses_i18n.map{ |k,v| { id: k, name: v } }
     gon.task_types = RelatedTask.task_types_i18n.map{ |k,v| { id: k, name: v } }
   end
