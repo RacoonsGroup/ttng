72cdeb037bea2ee97ffc5781d8206c2d1599c58a
Anton Zhirkov <an.zhirkov@gmail.com>
2015-05-21 11:52:59 +0400

    order for Project#index

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,7 +7,7 @@ class ProjectsController < AuthenticatedController
   before_filter :find_tasks, only: [:show, :to_google_drive]
 
   def index
-    @projects = ProjectPresenter.map(@projects.search(params[:search])).paginate(page: params[:page], per_page: 10)
+    @projects = ProjectPresenter.map(@projects.search(params[:search]).order('projects.created_at DESC')).paginate(page: params[:page], per_page: 10)
   end
 
   def new
