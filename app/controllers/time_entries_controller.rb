f8fb8108d65130a74fe7be28d2d9989b0269bc29
Anton Zhirkov <an.zhirkov@gmail.com>
2015-05-28 11:48:52 +0400

    Destroy TimeEntry instead of RelatedTask

--- app/controllers/time_entries_controller.rb
+++ app/controllers/time_entries_controller.rb
@@ -2,6 +2,7 @@ class TimeEntriesController < ApplicationController
   inject :time_entry_manager
 
   before_filter :find_task
+  before_filter :find_time_entry, only: :destroy
 
   def create
     authorize! :create, TimeEntry
@@ -14,13 +15,30 @@ class TimeEntriesController < ApplicationController
     end
   end
 
+  def destroy
+    time_entry_manager.destroy(@time_entry)
+    redirect_to :back
+  end
+
   protected
 
   def find_task
-    @related_task = current_user.related_tasks.find(params[:related_task_id])
+    @related_task = if current_user.chief?
+                    RelatedTask.find(params[:related_task_id])
+                  else
+                    current_user.related_tasks.find(params[:related_task_id])
+                  end
   end
 
   def create_task_params
     TimeEntryPermitter.permit(params)
+  end
+
+  def find_time_entry
+    @time_entry = if current_user.chief?
+                    TimeEntry.find(params[:id])
+                  else
+                    current_user.time_entries.find(params[:id])
+                  end
   end
 end
